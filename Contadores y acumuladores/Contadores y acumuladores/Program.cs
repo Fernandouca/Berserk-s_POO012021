c5efcc69b7e82f19e282eabaa1b2b5f33f1f5d92
luisfernandocalvillo <luiscalvillo2184@gmail.com>
2021-03-09 19:05:57 -0600

    Add: Contadores y acumuladores

---
+++ Contadores y acumuladores/Contadores y acumuladores/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Contadores_y_acumuladores
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a = 0;
+            int b = 0;
+            
+            Console.WriteLine("\n.:Bienvenido:.\n");
+            Console.Write("inserte un valor para a: ");
+            a = Int32.Parse(Console.ReadLine());
+            Console.Write("inserte un valor para b: ");
+            b = Int32.Parse(Console.ReadLine());
+            
+            Console.WriteLine($"Sumando {a} + {b} es igual a {AddNumbers(a,b)} ");
+        }
+        public static int AddNumbers(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
